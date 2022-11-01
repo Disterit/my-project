3d1170ddc1613e0911814335e5c109503b3e4722
Dister <71532605+Disterit@users.noreply.github.com>
2022-11-01 17:42:15 +0700

    Update eular_task_35.py

--- eular_task_35.py
+++ eular_task_35.py
@@ -1,57 +1,77 @@
-import itertools
-import time
+import json
+from bs4 import BeautifulSoup
+import lxml
+import requests
 
-def cirle_number(n):
-    start_time = time.time()
-    end_list = []
-    list_simple_numbers = []
-    for i in range(1, n+1,2):
-        lsit_circle_nubmer = []
-        i_round = round(i**0.5)
-
-        for d in range(2, i_round+1):
-            if i%d==0:
-                lsit_circle_nubmer.append(i)
-
-        if len(lsit_circle_nubmer) == 0:
-            list_simple_numbers.append(i)
-
-    for z in list_simple_numbers:
-        list_for_number = []
-        z = str(z)
-
-        for j in z:
-            list_for_number.append(int(j))
-
-        a = list(set(itertools.permutations(list_for_number)))
-        len_a = len(list_for_number)
-        cnt_a = 0
-        for i in a:
-            b = ''
-
-            for j in i:
-                b += str(j)
-
-            cnt = 0
-            c = int(b)
-            b = round(int(b) ** 0.5)
-
-            for d in range(2, b+1):
-                if c%d==0:
-                    cnt += 1
-
-            if cnt == 0:
-                cnt_a += 1
-                if cnt_a == len_a:
-                    end_list.append(int(z))
-    time_program = ((time.time() - start_time))
-    return len(end_list), time_program
+count = 0
+for i in range(1,3):
+    url = f"https://phil-nsk.ru/festivali/festivali/rossiya-muzyka-sibir/?PAGEN_1={i}"
 
 
+    list_fest_urls = []
+
+    headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+    }
 
 
+    req = requests.get(url=url, headers=headers)
+    src = req.text
+
+    soup = BeautifulSoup(src, "lxml")
+
+    url_fest = soup.find_all("a", class_="name")
+
+    for url_fests in url_fest:
+        fest_page = url_fests.get("href")
+        full_fest_page = "https://phil-nsk.ru" + fest_page
+        list_fest_urls.append(full_fest_page)
 
 
+    list_afisha_urls = []
 
-if __name__ == "__main__":
-    print(cirle_number(100000))
+
+    for list_url_festivals in list_fest_urls:
+        q = requests.get(list_url_festivals, headers=headers)
+        soup_1 = q.text
+        soup = BeautifulSoup(soup_1, "lxml")
+
+        block_festival = soup.find_all(class_="g-left item")
+
+        list_for_urls_fest = []
+
+        for item in block_festival:
+            fest_block_urls = item.find(class_="name-in").get("href")
+            fest_http = "https://phil-nsk.ru" + fest_block_urls
+            list_for_urls_fest.append(fest_http)
+
+
+        for urls_fest in list_for_urls_fest:
+            req = requests.get(urls_fest, headers=headers)
+            soup = BeautifulSoup(req.text, "lxml")
+
+            fest_list_result = []
+
+            name_festival = soup.find("h1").text
+            date_festival = soup.find("div", class_="date g-left").text
+            price_festival = soup.find("span", class_="price")
+            if price_festival == None:
+                continue
+            else:
+                price_festival = price_festival.text.strip()
+
+
+            fest_list_result.append(
+                {
+                    "name_festival": name_festival,
+                    "date_festival": date_festival,
+                    "price_festival": price_festival,
+                }
+            )
+
+
+            with open("fest_list_result.json",  "a", encoding="utf-8") as file:
+                json.dump(fest_list_result, file, indent=4, ensure_ascii=False)
+            count += 1
+            print(f"Fest parsing:{count}")
